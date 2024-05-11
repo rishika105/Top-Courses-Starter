b2c946f6deaec30008f764733321b63d9a5f50db
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2024-05-11 20:38:35 +0530

    initial commit

---
+++ src/Components/Spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+    return (
+        <div className="flex flex-col items-center space-y-2">
+            <div className="spinner">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <p className="text-bgDark text-lg font-semibold">Loading....</p>
+        </div>
+    );
+}
+
+export default Spinner;
+
+
